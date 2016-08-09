c0c19345eeff30eadada1ab31e16b9a8dd858960
haxwithaxe <spam@haxwithaxe.net>
2016-08-09 14:53:43 -0400

    added setup.py, module make target, and script to run worklog.

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+	name = 'worklog',
+	version = '0.1',
+	description = '',
+	author = '',
+	author_email = '',
+	url = '',
+	packages = [ 'worklog' ],
+	)
